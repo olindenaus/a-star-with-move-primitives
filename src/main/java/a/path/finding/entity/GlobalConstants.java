e95f8db8638e0466e22b8b1e416182309ba45cda
berkayersoy <berkayersoy17@gmail.com>
2020-01-12 11:48:55 +0100

    weird shapes examples commit

--- src/main/java/a/path/finding/entity/GlobalConstants.java
+++ src/main/java/a/path/finding/entity/GlobalConstants.java
@@ -2,7 +2,7 @@ package a.path.finding.entity;
 
 public class GlobalConstants {
 
-    public static int SIZE = 20;
+    public static int SIZE = 10;
     public static int TIME_INTERVAL = 1;
     public static int RESOLUTION_PENALTY = 20;
     public static int ORIGINAL_STEP_SIZE = 6;
